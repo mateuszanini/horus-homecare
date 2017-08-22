e19a45e3f2edd425e262467146339b450866bace
Mateus Zanini <mateus-zanini@hotmail.com>
2017-08-22 02:44:25 -0300

    Template inicial Login

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,36 @@
 <template>
     <div id="app">
-        <v-app id="example-2" toolbar>
-            <v-navigation-drawer temporary v-model="drawer" :mini-variant.sync="mini" light overflow absolute>
-                <v-list class="pa-1">
-                    <v-list-tile avatar tag="div">
-                        <v-list-tile-avatar>
-                            <img src="https://lh3.googleusercontent.com/2IB45LAqrh2duE_5hC9_gARcpA2P2Ee9DZiuDplD6uptqpsaEtbaa9ohgIpMG8b49PyCJ_2kwnW71A=w1920-h1080-rw-no" />
-                        </v-list-tile-avatar>
-                        <v-list-tile-content>
-                            <v-list-tile-title>Mateus Zanini</v-list-tile-title>
-                        </v-list-tile-content>
-                        <v-list-tile-action>
-                            <v-btn icon dark @click.native.stop="mini = !mini">
-                                <v-icon>chevron_left</v-icon>
-                            </v-btn>
-                        </v-list-tile-action>
-                    </v-list-tile>
-                </v-list>
-                <v-list class="pt-0" dense>
-                    <v-divider></v-divider>
-                    <v-list-tile v-for="item in items" :key="item.title">
-                        <v-list-tile-action>
-                            <v-icon>{{ item.icon }}</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-content>
-                            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </v-list>
-            </v-navigation-drawer>
-            <v-toolbar fixed class="teal" dark>
-                <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-                <v-toolbar-title>{{ titulo }}</v-toolbar-title>
+        <v-card flat>
+            <v-toolbar dark class="primary teal elevation-0" extended>
+
             </v-toolbar>
-            <main>
-                <v-container fluid>
-                    <v-list>
-                        <v-list-tile v-for="tecnologia in tecnologias" v-bind:key="tecnologia">
-                            <v-list-tile-action>
-                                <v-icon class="teal--text">arrow_right</v-icon>
-                            </v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title v-text="tecnologia"></v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </v-list>
-                    <!--v-router-->
-                </v-container>
-            </main>
-        </v-app>
+            <v-layout row>
+                <v-flex xs4 offset-xs4>
+                    <v-card class="card--flex-toolbar">
+                        <v-card-text class="white" style="height: 400px;">
+                            <div class="text-xs-center">
+                                <v-btn error dark block large :loading="loading" @click.native="loader = 'loading'" :disabled="loading" class="white--text">
+                                    <v-icon left dark>fa-google</v-icon>
+                                    Login com Google
+                                </v-btn>
+                                <v-btn primary dark block large :loading="loading2" @click.native="loader = 'loading2'" :disabled="loading2" class="white--text">
+                                    <v-icon left dark>fa-facebook-official</v-icon>
+                                    Login com Facebook
+                                </v-btn>
+                            </div>
+                        </v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-card>
+        <v-footer class="pa-3 teal white--text" fixed>
+            <div>© {{ new Date().getFullYear() }} Horus Homecare
+            </div>
+        </v-footer>
     </div>
 </template>
 
 <script>
-// import Hello from './components/Hello'
 
 export default {
     name: 'app',
@@ -67,15 +44,69 @@ export default {
             ],
             tecnologias: ['PWA', 'VueJS', 'Vuetify', 'VueFire', 'Firebase'],
             mini: false,
-            right: null
+            right: null,
+            //botão
+            loader: null,
+            loading: false,
+            loading2: false,
         }
     },
-    components: {
-        // Hello
-    }
+    watch: {
+        loader() {
+            const l = this.loader
+            this[l] = !this[l]
+
+            setTimeout(() => (this[l] = false), 3000)
+
+            this.loader = null
+        }
+    },
 }
 </script>
 
 <style>
+.card--flex-toolbar {
+    margin-top: -64px;
+}
 
+.custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+}
+
+@-moz-keyframes loader {
+    from {
+        transform: rotate(0);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+
+@-webkit-keyframes loader {
+    from {
+        transform: rotate(0);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+
+@-o-keyframes loader {
+    from {
+        transform: rotate(0);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+
+@keyframes loader {
+    from {
+        transform: rotate(0);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
 </style>
