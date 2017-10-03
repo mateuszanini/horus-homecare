0adcbd64d048d1d7f16b7aaaf318a78078761254
Mateus Zanini <mateus-zanini@hotmail.com>
2017-10-02 23:18:24 -0300

    Criado componente para snackbar

---
+++ src/components/shared/snackbar/Snackbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-snackbar :timeout="timeout" :success="context === 'success'" :info="context === 'info'" :warning="context === 'warning'" :error="context === 'error'" :primary="context === 'primary'" :secondary="context === 'secondary'" :multi-line="mode === 'multi-line'" :vertical="mode === 'vertical'" v-model="snackbar">
+        {{ text }}
+        <v-btn dark flat @click.native="snackbar = false" class="white--text">Ok</v-btn>
+    </v-snackbar>
+</template>
+
+<script>
+export default {
+    data(){
+        return {     
+            snackbar: true,
+            timeout: 4000,
+            mode: 'vertical'
+        }
+    },
+    props: {
+        context: {
+            type: String,
+            required: true
+        },
+        text:{
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
